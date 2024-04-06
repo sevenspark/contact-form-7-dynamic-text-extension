95bb7c7f5ac07250ce63615ca544f3477eb30542
Tessa <tessa@aurisecreative.com>
2024-04-06 14:12:36 -0400

    introduced dynamic attributes

--- contact-form-7-dynamic-text-extension.php
+++ contact-form-7-dynamic-text-extension.php
@@ -4,6 +4,7 @@
  * Plugin Name: Contact Form 7 - Dynamic Text Extension
  * Description: Extends Contact Form 7 by adding dynamic form fields that accepts shortcodes to prepopulate form fields with default values and dynamic placeholders.
  * Version: VERSION_PLACEHOLDER
+ * Text Domain: contact-form-7-dynamic-text-extension
  * Author: AuRise Creative, SevenSpark
  * Author URI: https://aurisecreative.com
  * Plugin URI: https://aurisecreative.com/products/wordpress-plugin/contact-form-7-dynamic-text-extension/
@@ -11,7 +12,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Requires at least: 5.5
  * Requires PHP: 7.4
- * Text Domain: contact-form-7-dynamic-text-extension
+ * Requires Plugins: contact-form-7
  *
  * @copyright Copyright (c) 2010-2024 Chris Mavricos, SevenSpark <https://sevenspark.com>
  * @copyright Copyright (c) 2022-2024 Tessa Watkins, AuRise Creative <https://aurisecreative.com>
@@ -287,26 +288,35 @@ function wpcf7dtx_shortcode_handler($tag)
     } else {
         $atts['aria-invalid'] = 'false';
     }
-    if ($tag->has_option('readonly')) {
-        $atts['readonly'] = 'readonly';
-    }
-
-    // Dynamically determine disabled attribute, remove if invalid
-    $atts['disabled'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('disabled', '', true)), ENT_QUOTES));
-    if ($atts['disabled'] != 'disabled') {
-        unset($atts['disabled']);
-    }
-
-    // Dynamically determine autofocus attribute, remove if invalid
-    $atts['autofocus'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('autofocus', '', true)), ENT_QUOTES));
-    if ($atts['autofocus'] != 'autofocus') {
-        unset($atts['autofocus']);
-    }
 
     // Add required attribute to applicable input types
     if ($tag->is_required() && !in_array($atts['type'], array('hidden', 'quiz'))) {
         $atts['aria-required'] = 'true';
         $atts['required'] = 'required';
+    }
+
+    /**
+     * Attributes for all fields
+     *
+     * Any attributes that are not allowed on specific elements will be stripped during escaping.
+     * See the `wpcf7dtx_get_allowed_field_properties()` for details.
+     */
+    $dynamic_atts = array('autofocus', 'disabled', 'readonly');
+    foreach ($dynamic_atts as $dynamic_att) {
+        // Don't override existing attributes
+        if (!array_key_exists($dynamic_att, $atts) && $tag->has_option($dynamic_att)) {
+            switch ($dynamic_att) {
+                default:
+                    $atts[$dynamic_att] = wpcf7dtx_get_dynamic(false, $tag, 'text', $dynamic_att); // Get dynamic attribute
+                    if ($atts[$dynamic_att] === '') {
+                        $atts[$dynamic_att] = $dynamic_att;  // Empty values are valid since boolean values just need to exist
+                    }
+                    if ($atts[$dynamic_att] !== $dynamic_att) {
+                        unset($atts[$dynamic_att]); // Remove attribute if it doesn't equal it's own name
+                    }
+                    break;
+            }
+        }
     }
 
     // Evaluate the dynamic value
@@ -372,38 +382,111 @@ function wpcf7dtx_shortcode_handler($tag)
             $atts['placeholder'] = wpcf7dtx_array_has_key('placeholder', $atts, __('&#8212;Please choose an option&#8212;', 'contact-form-7-dynamic-text-extension'));
         }
         if ($atts['type'] == 'select') {
-            $atts['size'] = $tag->get_size_option('1');
+            $atts['size'] = wpcf7dtx_get_dynamic(false, $tag, 'text', 'size'); // Get dynamic attribute
+            if ($atts['size'] === '') {
+                $atts['size'] = $tag->get_size_option('1'); // Set default value
+            }
         }
     } else {
         /**
-         * Configuration for text-based fields
+         * Attributes for text-based fields
+         *
+         * Any attributes that are not allowed on specific elements will be stripped during escaping.
+         * See the `wpcf7dtx_get_allowed_field_properties()` for details.
          */
-        $atts['size'] = $tag->get_size_option('40');
-        $atts['list'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('list', '', true)), ENT_QUOTES));
-        $atts['autocapitalize'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('autocapitalize', '', true)), ENT_QUOTES));
-        $atts['pattern'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('pattern', '', true)), ENT_QUOTES));
-        $atts['min'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('min', '', true)), ENT_QUOTES));
-        $atts['max'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('max', '', true)), ENT_QUOTES));
-        $atts['step'] = wpcf7dtx_get_dynamic(html_entity_decode(urldecode($tag->get_option('step', '', true)), ENT_QUOTES));
+        $dynamic_atts = array('autocapitalize', 'autocomplete', 'cols', 'list', 'max', 'maxlength', 'min', 'minlength', 'pattern', 'rows', 'size', 'step');
+        foreach ($dynamic_atts as $dynamic_att) {
+            // Don't override existing attributes
+            if (!array_key_exists($dynamic_att, $atts) && $tag->has_option($dynamic_att)) {
+                $atts[$dynamic_att] = wpcf7dtx_get_dynamic(false, $tag, 'text', $dynamic_att); // Get dynamic attribute
+                switch ($dynamic_att) {
+                    case 'autocapitalize':
+                        if (!in_array($atts[$dynamic_att], array('none', 'off', 'on', 'sentences', 'words', 'characters'))) {
+                            unset($atts[$dynamic_att]); // Remove if invalid
+                        }
+                        break;
+                    case 'autocomplete':
+                        // Autocomplete attribute
+                        if ($atts['type'] == 'hidden') {
+                            $atts['autocomplete'] = 'off'; // Always disable for hidden fields
+                        } else {
+                            // Disable autocomplete for this field if a value has been specified
+                            $atts['autocomplete'] = $atts['value'] ? 'off' : $atts['autocomplete']; // Get dynamic attribute // Get dynamic attribute
+                        }
+                        break;
+                    case 'maxlength':
+                        if ($atts[$dynamic_att] === '' || !is_numeric($atts[$dynamic_att])) {
+                            $atts[$dynamic_att] = $tag->get_maxlength_option(); // Set default if empty or invalid
+                        }
+                        break;
+                    case 'minlength':
+                        if ($atts[$dynamic_att] === '' || !is_numeric($atts[$dynamic_att])) {
+                            $atts[$dynamic_att] = $tag->get_minlength_option(); // Set default if empty or invalid
+                        }
+                        break;
+                    case 'cols':
+                        if ($atts[$dynamic_att] === '' || !is_numeric($atts[$dynamic_att])) {
+                            $atts[$dynamic_att] = $tag->get_cols_option('40'); // Set default if empty or invalid
+                        }
+                        break;
+                    case 'rows':
+                        if ($atts[$dynamic_att] === '' || !is_numeric($atts[$dynamic_att])) {
+                            $atts[$dynamic_att] = $tag->get_rows_option('10'); // Set default if empty or invalid
+                        }
+                        break;
+                    case 'size':
+                        if ($atts[$dynamic_att] === '' || !is_numeric($atts[$dynamic_att])) {
+                            $atts[$dynamic_att] = $tag->get_size_option('40'); // Set default if empty or invalid
+                        }
+                        break;
+                    case 'wrap':
+                        if (!in_array($atts[$dynamic_att], array('hard', 'soft'))) {
+                            unset($atts[$dynamic_att]); // Remove if invalid
+                        }
+                        break;
+                    case 'max':
+                    case 'min':
+                        // Do nothing
+                        break;
+                    default:
+                        if ($atts[$dynamic_att] === '') {
+                            unset($atts[$dynamic_att]); // Remove attribute if empty
+                        }
+                        break;
+                }
+            }
+        }
 
-        // Min and Max length attributes
-        $atts['maxlength'] = $tag->get_maxlength_option();
-        $atts['minlength'] = $tag->get_minlength_option();
-        if ($atts['maxlength'] && $atts['minlength'] && $atts['maxlength'] < $atts['minlength']) {
+        // Validate Min and Max length attributes (should always be numeric)
+        if ($atts['maxlength'] && $atts['minlength'] && intval($atts['maxlength']) < intval($atts['minlength'])) {
             unset($atts['maxlength'], $atts['minlength']);
-        }
-
-        // Autocomplete attribute
-        if ($atts['type'] == 'hidden') {
-            $atts['autocomplete'] = 'off'; // Always disable for hidden fields
         } else {
-            // Disable autocomplete for this field if a dynamic value has been specified
-            $atts['autocomplete'] = $atts['value'] ? 'off' : $tag->get_option('autocomplete', '[-0-9a-zA-Z]+', true);
+            /**
+             * The `maxlength` attribute must be an integer with a value of 0 or higher
+             *
+             * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/maxlength
+             */
+            if (!is_numeric($atts['maxlength']) || intval($atts['maxlength']) < 0) {
+                unset($atts['maxlength']);
+            }
+            /**
+             * The `minlength` attribute must be an integer with a value of 0 or higher
+             *
+             * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/minlength
+             */
+            if (!is_numeric($atts['minlength']) || intval($atts['minlength']) < 0) {
+                unset($atts['minlength']);
+            }
         }
 
+        // Validate Min and Max attributes if numeric
+        if (is_numeric($atts['max']) && is_numeric($atts['min']) && floatval($atts['max']) < floatval($atts['min'])) {
+            unset($atts['max'], $atts['min']);
+        }
+
+        // Client-side validation by type
         switch ($atts['type']) {
             case 'range':
-                // Client-side validation by type
                 $atts['class'][] =  'wpcf7-validates-as-number';
                 break;
             case 'date':
@@ -411,17 +494,7 @@ function wpcf7dtx_shortcode_handler($tag)
             case 'email':
             case 'url':
             case 'tel':
-                // Client-side validation by type
                 $atts['class'][] =  sanitize_html_class('wpcf7-validates-as-' . $atts['type']);
-                break;
-            case 'textarea':
-                // Attributes unique to textareas
-                $atts['cols'] = $tag->get_cols_option('40');
-                $atts['rows'] = $tag->get_rows_option('10');
-                $atts['wrap'] = $tag->get_option('wrap', '', true);
-                if (!in_array($atts['wrap'], array('hard', 'soft'))) {
-                    unset($atts['wrap']);
-                }
                 break;
         }
     }
