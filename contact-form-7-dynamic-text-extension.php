e5008db1da4eb13a5918f207a7e8a295b5e9c5d6
Tessa <tessa@aurisecreative.com>
2024-04-09 09:28:44 -0400

    Fix undefined array key errors introduced in 4.4.0

--- contact-form-7-dynamic-text-extension.php
+++ contact-form-7-dynamic-text-extension.php
@@ -444,10 +444,6 @@ function wpcf7dtx_shortcode_handler($tag)
                             unset($atts[$dynamic_att]); // Remove if invalid
                         }
                         break;
-                    case 'max':
-                    case 'min':
-                        // Do nothing
-                        break;
                     default:
                         if ($atts[$dynamic_att] === '') {
                             unset($atts[$dynamic_att]); // Remove attribute if empty
@@ -458,7 +454,7 @@ function wpcf7dtx_shortcode_handler($tag)
         }
 
         // Validate Min and Max length attributes (should always be numeric)
-        if ($atts['maxlength'] && $atts['minlength'] && intval($atts['maxlength']) < intval($atts['minlength'])) {
+        if (array_key_exists('maxlength', $atts) && $atts['maxlength'] && array_key_exists('minlength', $atts) && $atts['minlength'] && intval($atts['maxlength']) < intval($atts['minlength'])) {
             unset($atts['maxlength'], $atts['minlength']);
         } else {
             /**
@@ -466,7 +462,7 @@ function wpcf7dtx_shortcode_handler($tag)
              *
              * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/maxlength
              */
-            if (!is_numeric($atts['maxlength']) || intval($atts['maxlength']) < 0) {
+            if (array_key_exists('maxlength', $atts) && (!is_numeric($atts['maxlength']) || intval($atts['maxlength']) < 0)) {
                 unset($atts['maxlength']);
             }
             /**
@@ -474,13 +470,13 @@ function wpcf7dtx_shortcode_handler($tag)
              *
              * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/minlength
              */
-            if (!is_numeric($atts['minlength']) || intval($atts['minlength']) < 0) {
+            if (array_key_exists('minlength', $atts) && (!is_numeric($atts['minlength']) || intval($atts['minlength']) < 0)) {
                 unset($atts['minlength']);
             }
         }
 
         // Validate Min and Max attributes if numeric
-        if (is_numeric($atts['max']) && is_numeric($atts['min']) && floatval($atts['max']) < floatval($atts['min'])) {
+        if (array_key_exists('max', $atts) && array_key_exists('min', $atts) && is_numeric($atts['max']) && is_numeric($atts['min']) && floatval($atts['max']) < floatval($atts['min'])) {
             unset($atts['max'], $atts['min']);
         }
 
